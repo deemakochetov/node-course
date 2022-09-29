75ba9a722085afa03e9a9f1282a095c0df7347c9
kochetovdmytro <kochetovdmytro@gmail.com>
2022-09-29 20:33:17 +0200

    Add axios, do first fetch to api

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,1 +1,21 @@
-a;
+const axios = require('axios').default;
+
+require('dotenv').config();
+
+const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_API_KEY}&query=New`;
+axios
+  .get(url)
+  .then((response) => {
+    const currentTemp = response.data.current.temperature;
+    const probabilityRain = response.data.current.precip;
+    console.log(currentTemp, probabilityRain);
+  })
+  .catch((error) => {
+    // handle error
+    console.log(error);
+  })
+  .then(() => {
+    // always executed
+  });
+
+// O
