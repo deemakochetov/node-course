678bfb20871b3ed99e988de163093deb0506010f
kochetovdmytro <kochetovdmytro@gmail.com>
2022-09-30 18:15:36 +0200

    move external apis to web-server directory

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 const { log, logFailure, logSuccess } = require('./utils/logging');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')));
+const partialsPath = path.join(__dirname, '../views/partials');
+const staticPath = path.join(__dirname, '../public');
+app.set('view engine', 'hbs');
+app.use(express.static(staticPath));
+
+hbs.registerPartials(partialsPath);
 
 app.get('', (req, res) => {
   res.send('App response');
---
+++ node-course/web-server/src/utils/geocoding.js
@@ -0,0 +1,25 @@
+const axios = require('axios').default;
+
+require('dotenv').config();
+
+const getGeocoding = (loc, callback) => {
+  const locationURL = encodeURIComponent(loc);
+  const url = `http://api.positionstack.com/v1/forward?access_key=${process.env.POSITIONSTACK_API_KEY}&query=${locationURL}`;
+  axios
+    .get(url)
+    .then((response) => {
+      if (response.error) callback('Unable to find location', undefined);
+      else {
+        const { latitude, longitude, label } = response.data.data[0];
+        callback(undefined, { latitude, longitude, label });
+      }
+    })
+    .catch(() => {
+      callback('Unable to connect to location services', undefined);
+    })
+    .then(() => {
+      // always executed
+    });
+};
+
+module.exports = getGeocoding;
